87a14f79a97905a26552c0d15a2daebbfcd56955
tobiasneyen <tobicabre21@gmail.com>
2025-06-02 01:12:50 -0300

    "loguin"

---
+++ login.js
@@ -0,0 +1,37 @@
+
+
+
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const nombre = document.getElementById('nombre').value.trim();
+    const correo = document.getElementById('correo').value.trim();
+    const clave = document.getElementById('clave').value.trim();
+
+    try {
+        const response = await fetch('base_de_datos.json');
+        if (!response.ok) throw new Error('Error al cargar el archivo JSON');
+        const data = await response.json();
+        const usuario = data.usuarios.find(user =>
+
+            user.nombre.toLowerCase() === nombre.toLowerCase() &&
+            user.correo.toLowerCase() === correo.toLowerCase() &&
+
+            user.PIN === clave
+        );
+
+        if (usuario) {
+
+
+            alert('¡Inicio de sesión exitoso! Bienvenido, ' + usuario.nombre);
+        } else {
+
+            alert('Error: Nombre, correo o contraseña incorrectos.');
+        }
+    } catch (error) {
+
+        console.error('Error:',   error) ;
+        alert('Ocurrió un error al procesar la solicitud. Por favor, intenta de nuevo.');
+
+
+    }
+});
